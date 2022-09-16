f52641a2d10c3e5c0b163773d6befd2b522ff80e
Kirill Aleksandrov <xakepgame@gmail.com>
2022-09-16 19:41:16 +0300

    Try to catch the button

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('test');
+const button = document.getElementById('button');
+const buttonHeight = 30;
+const buttonWidth = 100;
+
+button.addEventListener('mouseover', () => {
+    button.style.top = Math.random() * (window.innerHeight - buttonHeight) + 'px';
+    button.style.left = Math.random() * (window.innerWidth - buttonWidth) + 'px';
+})
